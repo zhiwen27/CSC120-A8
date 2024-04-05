ea5313f398675e99fd991c06d02b588fc5c20d4d
Zhiwen Ji <zji@smith.edu>
2024-04-04 20:10:46 -0400

    Some Undo

--- Player.java
+++ Player.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
 import java.lang.Math;
-import java.util.Scanner;
 public class Player implements Contract {
 
     private String name;
@@ -12,6 +12,7 @@ public class Player implements Contract {
     private double strength;
     protected Hashtable<String, Integer> inventory;
     protected int maxCapacity;
+    protected ArrayList<Player> Undo = new ArrayList<>();
 
     /**
      * Default Constructor: Intialize the inventory of the user and set its maxcapacity to be 10
@@ -26,6 +27,7 @@ public class Player implements Contract {
         this.size = 10;
         this.strength = 50.0;
         this.inventory = new Hashtable<>(this.maxCapacity);
+        Undo.add(this);
     }
 
     /**
@@ -35,6 +37,7 @@ public class Player implements Contract {
     public Player(String name){
         this();
         this.name = name;
+        Undo.add(this);
     }
 
     /**
@@ -46,6 +49,7 @@ public class Player implements Contract {
         this();
         this.name = name;
         this.maxCapacity = max;
+        Undo.add(this);
     }
 
     /**
@@ -59,6 +63,7 @@ public class Player implements Contract {
         this.name = name;
         this.x = x;
         this.y = y;
+        Undo.add(this);
     }
 
     /**
@@ -74,6 +79,7 @@ public class Player implements Contract {
         this.x = x;
         this.y = y;
         this.size = size;
+        Undo.add(this);
     }
 
     /**
@@ -98,11 +104,14 @@ public class Player implements Contract {
                     utilizability += 50;    // assuming getting the same time would add 50 more utilizability to the item
                     this.strength -= 5.0;
                     this.inventory.put(item, utilizability);
+                    Undo.add(this);
                     System.out.println("Great! Your " + item + " now has " + utilizability + " utilizability!");
                 }
                 else{
                     this.strength -= 5.0;
                     this.inventory.put(item, 100);    // initializing each item with 100 utilizability
+                    Player newPlayer = this;
+                    Undo.add(newPlayer);
                     System.out.println("Congratulations! You have discovered a new item: " + item + "!" + " Your " + item + " now has 100 utilizability!");
                 }
             }
@@ -126,9 +135,11 @@ public class Player implements Contract {
                 if (utilizability == 0){
                     this.strength -= 5.0;
                     this.inventory.remove(item);
+                    Undo.add(this);
                     return "Wise choice! You have successfully remove " + item + " from your inventory.";
                 }
                 else{
+                    Undo.add(this);
                     return "Your " + item + " still has " + utilizability + " utilizability... Do you really want to drop it?";
                 }
             }
@@ -141,12 +152,23 @@ public class Player implements Contract {
     /**
      * Print the items inside the inventory
      */
-    public void printItem(){
-        Enumeration<String> keys = this.inventory.keys();
-        int num = 1;    // Simply indicate the number of the item
-        while(keys.hasMoreElements()){
-            System.out.println("Item " + num + ": " + keys.nextElement());
-            num++;
+    public String printItem(){
+        if (this.inventory.isEmpty()){
+            return "Nothing! Start your discovery now!";
+        }
+        else{
+            Enumeration<String> keys = this.inventory.keys();
+            int num = this.inventory.size();    // Simply indicate the number of the item
+            ArrayList<String> s = new ArrayList<>();
+            while(keys.hasMoreElements()){
+                s.add("Item " + num + ": " + keys.nextElement() + "\n");
+                num--;
+            }
+            String print = "";
+            for (int i = s.size() - 1; i >= 0; i--){
+                print += s.get(i);
+            }
+            return print;
         }
     }
 
@@ -158,10 +180,12 @@ public class Player implements Contract {
         if (checkStrength()){
             if (this.inventory.containsKey(item)){
                 this.strength -= 2.0;
+                Undo.add(this);
                 System.out.println("Your " + item + " now has " + this.inventory.get(item) + " utilizability!");
             }
             else{
                 this.strength -= 2.0;
+                Undo.add(this);
                 System.out.println("Sorry, you don't have " + item + "yet, try discover it!");
             }
         }
@@ -180,6 +204,7 @@ public class Player implements Contract {
             if (utilizability > 0){
                 this.strength -= 5.0;
                 utilizability -= 10;    // assuming using the item each time would reduce its utilizability by 10;
+                Undo.add(this);
                 this.inventory.put(item,utilizability);
             }
             else{
@@ -206,49 +231,58 @@ public class Player implements Contract {
             if (direction.contains("right") || direction.contains("east")){
                 this.x += 1;
                 this.strength -= 10.0;
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else if (direction.contains("left") || direction.contains("west")){
                 this.x -= 1;
                 this.strength -= 10.0;
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else if (direction.contains("up") || direction.contains("north")){
                 this.y += 1;
                 this.strength -= 10.0;
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else if (direction.contains("down") || direction.contains("south")){
                 this.y -= 1;
                 this.strength -= 10.0;
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else if (direction.contains("upper-right") || direction.contains("north-east")){
                 this.x += 1;
                 this.y += 1;
                 this.strength -= 10.0 * Math.sqrt(2);
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else if (direction.contains("upper-left") || direction.contains("north-west")){
                 this.x -= 1;
                 this.y += 1;
                 this.strength -= 10.0 * Math.sqrt(2);
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else if (direction.contains("lower-right") || direction.equals("south-east")){
                 this.x += 1;
                 this.y -= 1;
                 this.strength -= 10.0 * Math.sqrt(2);
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else if (direction.contains("lower-left") || direction.equals("south-west")){
                 this.x -= 1;
                 this.y -= 1;
                 this.strength -= 10.0 * Math.sqrt(2);
+                Undo.add(this);
                 System.out.println("You're now at (" + this.x + "," + this.y + ").");
             }
             else{
                 this.strength -= 10.0;
+                Undo.add(this);
                 System.out.println("You're now walking to the " + direction + ".");
             }
         }
@@ -266,6 +300,7 @@ public class Player implements Contract {
             this.x -= x;
             this.y -= y;
             this.strength -= 10.0 * Math.sqrt(x * x + y * y);
+            Undo.add(this);
             System.out.println("You're now at (" + this.x + "," + this.y + ").");
         }
         System.out.println("You don't have enough strength to fly any more. Take a rest!");
@@ -276,14 +311,18 @@ public class Player implements Contract {
      * Shrink by a factor of two: return the new size
      */
     public Number shrink(){
-        return this.size / 2;
+        this.size /= 2;
+        Undo.add(this);
+        return this.size;
     }
 
     /**
      * Grow by a factor of two: return the new size
      */
     public Number grow(){
-        return this.size * 2;
+        this.size *= 2;
+        Undo.add(this);
+        return this.size;
     }
 
     /**
@@ -291,20 +330,37 @@ public class Player implements Contract {
      */
     public void rest(){
         this.strength += 50;
+        Undo.add(this);
         System.out.println("You're now having a rest.");
     }
 
+    /**
+     * Print out basic information about the player
+     */
+    public String toString(){
+        String s = "\n";
+        s += "Your name: " + this.name + ". \nYour size: " + this.size + ". \nYour strength: " + this.strength + ". \nYou're now at: (" + this.x + "," + this.y + ").\n";
+        s += "Your inventory has: \n";
+        s += this.printItem();
+        return s;
+    }
+
+    /**
+     * Undo: get back to the previous step and throw an exception
+     */
     public void undo(){
-        
+        Undo.removeLast();
+        System.out.println(Undo.getLast());
+        // throw new RuntimeException("You have know undone your process. Please start over!");
     }
 
     public static void main(String[] args) {
         Player newPlayer = new Player("A");
-        newPlayer.grab("Thing");
-        newPlayer.grab("a");
-        newPlayer.grab("B");
-        newPlayer.printItem();
-        newPlayer.examine("Thing");
-        System.out.println(newPlayer.drop("a"));
+        newPlayer.grab("Item 1");
+        newPlayer.undo();
+        //newPlayer.grab("B");
+        //System.out.println(newPlayer.printItem());
+        //newPlayer.examine("Thing");
+        //System.out.println(newPlayer.drop("a"));
     }
 }
--- reflection.md
+++ reflection.md
@@ -1,5 +1,10 @@
 Use this file to record your reflection on this assignment.
 
 - Tell me about your class! What does it represent?
+My class represent a new player, which has basic attributes of name, size, strength, starting location and a new inventory with max capacity that can be modified. Each item in the inventory has a key value of its name and its corresponding utilizability.
 - What additional methods (if any) did you implement alongside those listed in the interface?
+The class has additional methods:
+checkStrength(), which returns whether the player has enough strength to carry on movements or simply need a rest
+printItem(), which print all the items in the inventory
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I am a bit confused about how to write undo() method.
